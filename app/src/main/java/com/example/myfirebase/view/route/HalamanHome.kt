d858374ccb1c973c681c445a1185a64f8fece839
Asti22 <uun3461@gmail.com>
2026-01-02 16:47:06 +0700

    OnLoading & OnError: Menampilkan indikator visual saat proses ambil data gagal atau sedang berjalan. OnError menyertakan tombol untuk mencoba lagi (retryAction).

--- app/src/main/java/com/example/myfirebase/view/route/HalamanHome.kt
+++ app/src/main/java/com/example/myfirebase/view/route/HalamanHome.kt
@@ -1,5 +1,6 @@
 package com.example.myfirebase.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.myfirebase.R
@@ -85,4 +87,13 @@ fun HomeBody(
             )
         }
     }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
 }
