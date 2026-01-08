6a80c2deb9dba258f75fe1310cf923e5d22dff76
Asti22 <uun3461@gmail.com>
2026-01-08 08:56:56 +0700

    implementasi konfirmasi hapus pada detail siswa

--- app/src/main/java/com/example/myfirebase/view/route/HalamanDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/route/HalamanDetail.kt
@@ -1,5 +1,7 @@
 package com.example.myfirebase.view.route
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -10,20 +12,28 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myfirebase.R
+import com.example.myfirebase.modeldata.Siswa
 import com.example.myfirebase.modeldata.toDataSiswa
 import com.example.myfirebase.viewmodel.DetailViewModel
 import com.example.myfirebase.viewmodel.PenyediaViewModel
+import com.google.androidbrowserhelper.playbilling.digitalgoods.ItemDetails
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -82,9 +92,51 @@ fun DetailSiswaScreen(
 }
 
 @Composable
-fun ItemDetailsBody(detailSiswa: toDataSiswa, onDelete: () -> launch, modifier: fillMaxWidth) {
-    TODO("Not yet implemented")
+private fun ItemDetailsBody(
+    detailSiswa: Siswa,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+        ItemDetails(
+            siswa = detailSiswa,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        // Tombol Hapus
+        OutlinedButton(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false }
+            )
+        }
+    }
 }
+
+@Composable
+fun ItemDetails(
+    siswa: Siswa,
+    modifier: Modifier = Modifier
+) {
+
+}
+
+
 
 
 
