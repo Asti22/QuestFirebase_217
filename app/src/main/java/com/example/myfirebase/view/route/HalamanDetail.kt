4f0011ac6c7fd8c2905ebdde3f5b4e639b500c3c
Asti22 <uun3461@gmail.com>
2026-01-08 08:47:50 +0700

    mengatur Scaffold dan navigasi kembali di detail siswa

--- app/src/main/java/com/example/myfirebase/view/route/HalamanDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/route/HalamanDetail.kt
@@ -1,12 +1,16 @@
 package com.example.myfirebase.view.route
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myfirebase.R
 import com.example.myfirebase.viewmodel.DetailViewModel
 import com.example.myfirebase.viewmodel.PenyediaViewModel
 
@@ -25,3 +29,14 @@ fun DetailSiswaScreen(
     LaunchedEffect(Unit) {
         viewModel.getSiswaById()
     }
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(R.string.detail_siswa),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior
+            )
+        },
+
