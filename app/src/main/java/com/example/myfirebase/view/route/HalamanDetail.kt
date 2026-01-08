50945c01e1996ea4afdea2e8fedf528d11db2433
Asti22 <uun3461@gmail.com>
2026-01-08 08:50:32 +0700

    implementasi edit dan hapus data siswa di halaman detail

--- app/src/main/java/com/example/myfirebase/view/route/HalamanDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/route/HalamanDetail.kt
@@ -1,6 +1,15 @@
 package com.example.myfirebase.view.route
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -8,11 +17,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myfirebase.R
+import com.example.myfirebase.modeldata.toDataSiswa
 import com.example.myfirebase.viewmodel.DetailViewModel
 import com.example.myfirebase.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,4 +51,40 @@ fun DetailSiswaScreen(
                 scrollBehavior = scrollBehavior
             )
         },
+        floatingActionButton = {
+            FloatingActionButton(
+                // Mengirim ID ke fungsi navigasi Edit
+                onClick = { navigateToEditItem(uiState.detailSiswa.id) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.edit_siswa)
+                )
+            }
+        }
+    ) { innerPadding ->
+        ItemDetailsBody(
+            detailSiswa = uiState.detailSiswa.toDataSiswa(),
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.deleteSiswa()
+                    navigateBack() // Kembali ke Home setelah hapus
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun ItemDetailsBody(detailSiswa: toDataSiswa, onDelete: () -> launch, modifier: fillMaxWidth) {
+    TODO("Not yet implemented")
+}
+
+
 
