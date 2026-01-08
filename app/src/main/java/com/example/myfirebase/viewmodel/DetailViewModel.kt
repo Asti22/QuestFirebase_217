f5f79470ee17430279441963780f3d92a12c849f
Asti22 <uun3461@gmail.com>
2026-01-08 08:12:25 +0700

    inisialisasi state dan panggil getSiswaById

--- app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -1,9 +1,19 @@
 package com.example.myfirebase.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.myfirebase.repositori.RepositorySiswa
 
 class DetailViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositorySiswa: RepositorySiswa
-)
+): ViewModel() {
+
+    private val itemId: String = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    var uiStateDetailSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    init { getSiswaById() }
+
+
